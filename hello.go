a3f0e39cfd3936b679fd627d4b94cf1bf4effd3c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-19 07:32:14 +0200

    Language Spanish supported

--- hello.go
+++ hello.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("%s, %s", englishHelloPrefix, name)
+	if language == "" {
+		language = "English"
+	}
+	prefix := ""
+	switch language {
+	case "English":
+		prefix = englishHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return fmt.Sprintf("%s, %s", prefix, name)
 }
--- hello_test.go
+++ hello_test.go
@@ -11,14 +11,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to a given name", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello uses the default World name when name is not given", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greeting in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
