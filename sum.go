81e10a330e98436a4bc8e87803d84c5a3f18a304
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-20 07:40:33 +0200

    Add range for summing up

--- sum.go
+++ sum.go
@@ -2,8 +2,8 @@ package main
 
 func Sum(arrayOfIntegers [4]int) (sum int) {
 	sum = 0
-	for i := 0; i < 4; i++ {
-		sum += arrayOfIntegers[i]
+	for _, v := range arrayOfIntegers {
+		sum += v
 	}
 	return
 }
