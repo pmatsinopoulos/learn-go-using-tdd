d91cdb594bbf6c1a56dd58a3fe18fd8f4d92f34c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-28 08:46:49 +0200

    Use switch statement to clean the code

--- reflection/walk.go
+++ reflection/walk.go
@@ -14,19 +14,17 @@ func reflectValue(i interface{}) reflect.Value {
 
 func walk(x interface{}, walkerFunc walkerFuncT) {
 	val := reflectValue(x)
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			walk(field.Interface(), walkerFunc)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), walkerFunc)
 		}
-	} else {
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-			switch field.Kind() {
-			case reflect.String:
-				walkerFunc(field.String())
-			case reflect.Struct:
-				walk(field.Interface(), walkerFunc)
-			}
-		}
+	case reflect.String:
+		walkerFunc(val.String())
 	}
 }
