7b9928b980598535e10b8464d71897e13d360c45
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-29 10:56:58 +0200

    Test for Counter#Val() and also a small refactoring in the tests

--- sync_tutorial/counter_test.go
+++ sync_tutorial/counter_test.go
@@ -3,6 +3,22 @@ package sync_tutorial
 import "testing"
 
 func TestCounter(t *testing.T) {
+	assertCounter := func(t testing.TB, counter Counter, numberOfIncrements int) {
+		t.Helper()
+
+		if counter.Val() != numberOfIncrements {
+			t.Errorf("Expected %v, got %v", numberOfIncrements, counter.Val())
+		}
+	}
+
+	t.Run("#Val() returns the internal counter", func(t *testing.T) {
+		counter := Counter{8}
+		got := counter.Val()
+		if got != 8 {
+			t.Errorf("Expected 8 got %v", got)
+		}
+	})
+
 	t.Run("incrementing the counter by 3 leaves it to 3", func(t *testing.T) {
 		counter := Counter{}
 		numberOfTimesToIncrement := 3
@@ -10,9 +26,6 @@ func TestCounter(t *testing.T) {
 			counter.Inc()
 		}
 
-		got := counter.Val()
-		if got != numberOfTimesToIncrement {
-			t.Errorf("Expected %v, got %v", numberOfTimesToIncrement, got)
-		}
+		assertCounter(t, counter, numberOfTimesToIncrement)
 	})
 }
