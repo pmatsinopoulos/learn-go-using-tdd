d46f01fe47d75d36bcc53ea4e0d0a31cc12ad16a
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-30 20:52:52 +0200

    Use constans for title and description separators

--- blogposts/post.go
+++ blogposts/post.go
@@ -10,14 +10,19 @@ type Post struct {
 	Title       string
 }
 
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+)
+
 func newPost(postFile io.Reader) (Post, error) {
 	scanner := bufio.NewScanner(postFile)
 	readLine := func() string {
 		scanner.Scan()
 		return scanner.Text()
 	}
-	title := readLine()[7:]
-	description := readLine()[13:]
+	title := readLine()[len(titleSeparator):]
+	description := readLine()[len(descriptionSeparator):]
 
 	post := Post{
 		Description: description,
