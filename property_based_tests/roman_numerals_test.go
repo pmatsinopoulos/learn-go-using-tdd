07c888b5355e2dc7572b81e78020d352c2e85493
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-01-30 12:32:44 +0200

    Arabic to Roman works from 1..19

--- property_based_tests/roman_numerals_test.go
+++ property_based_tests/roman_numerals_test.go
@@ -7,9 +7,27 @@ import (
 
 func TestRomanNumerals(t *testing.T) {
 	testCases := map[int]string{
-		1: "I",
-		2: "II",
-		3: "III",
+		1:  "I",
+		2:  "II",
+		3:  "III",
+		4:  "IV",
+		5:  "V",
+		6:  "VI",
+		7:  "VII",
+		8:  "VIII",
+		9:  "IX",
+		10: "X",
+		11: "XI",
+		12: "XII",
+		13: "XIII",
+		14: "XIV",
+		15: "XV",
+		16: "XVI",
+		17: "XVII",
+		18: "XVIII",
+		19: "XIX",
+		20: "XX",
+		39: "XXXIX",
 	}
 	for arabicNumber, romanNumber := range testCases {
 		testName := fmt.Sprintf("%d gets converted to %q", arabicNumber, romanNumber)
