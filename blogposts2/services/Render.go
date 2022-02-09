ce10846dc9d3a9b2eeacb864e4172d1cee1c100b
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-02-09 08:51:04 +0200

    Read from the file system

--- blogposts2/services/Render.go
+++ blogposts2/services/Render.go
@@ -2,23 +2,26 @@ package services
 
 import (
 	"bytes"
+	"embed"
 	"github.com/pmatsinopoulos/blogposts2/models"
 	"html/template"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
 )
 
 // Writes data to the buffer. The data should be the HTML representation
 // of the +Post+
 
 func Render(buf *bytes.Buffer, post models.Post) (err error) {
-	templateStr := `<h1>{{.Title}}</h1>
-<p>{{.Description}}</p>
-Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
-
 	var parsed *template.Template
-	parsed, err = template.New("blog").Parse(templateStr)
+	parsed, err = template.ParseFS(postTemplates, "templates/*.go.html")
 	if err != nil {
 		return err
 	}
+
 	err = parsed.Execute(buf, post)
 
 	return err
