8cc7c4c5d6d7b03df1bbe288ef4dcd2a4c09398b
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-02-19 12:47:57 +0200

    Added integration test InMemoryPlayerStore and PlayerServer

--- players/main.go
+++ players/main.go
@@ -6,19 +6,8 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct {
-}
-
-func (i InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i InMemoryPlayerStore) RecordWin(name string) {
-
-}
-
 func main() {
-	playerStore := InMemoryPlayerStore{}
+	playerStore := v1.NewInMemoryPlayerStore()
 	server := v1.PlayerServer{PlayerStore: playerStore}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
---
+++ players/v1/tests/server_integration_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	v1 "github.com/pmatsinopoulos/players/v1"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := v1.NewInMemoryPlayerStore()
+	server := v1.PlayerServer{PlayerStore: store}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+	assertResponseBody(t, response.Body.String(), "3")
+}
